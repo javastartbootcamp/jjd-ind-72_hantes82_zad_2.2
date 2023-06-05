495ac1cd1796521db63c65a5ff2e664329cffd82
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-06-05 09:51:13 +0200

    Zadanie 2.2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,14 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        String nameProdFirst = "Mleko";
+        String producerFirst = "Mlekowita";
+        double prizeFirst = 2.5;
+        String nameProdSec = "Czekolada";
+        String producerSec = "Wedel";
+        double prizeSec = 2.19;
 
+        System.out.println(nameProdFirst + " " + producerFirst + " " + prizeFirst +"zł");
+        System.out.println(nameProdSec + " " + producerSec + " " + prizeSec +"zł");
     }
 }
